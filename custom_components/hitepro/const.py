de20c7435f67f6dbb135f90382949022386f6b9e
Konstantin <edeminteractive@gmail.com>
2023-11-10 22:48:43 +0300

    version string in mainfest, domain register

--- custom_components/hitepro/const.py
+++
@@ -1,9 +0,0 @@
-"""Constants for the Hite-Pro Bridge integration."""
-
-DOMAIN = "hitepro"
-CONF_IDENTIFIER = "identifier"
-CONF_HOST = "host"
-CONF_USERNAME = "username"
-CONF_PASSWORD = "password"
-VERBOSE = True
-
