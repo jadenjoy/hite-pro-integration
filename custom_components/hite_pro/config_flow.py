cf1a97ca79f5594948d9a123c776469473f8c917
Konstantin <edeminteractive@gmail.com>
2023-11-11 04:32:19 +0300

    Config flow fix

--- custom_components/hite_pro/config_flow.py
+++ custom_components/hite_pro/config_flow.py
@@ -1,101 +1,66 @@
-"""Config flow for Hite-Pro Bridge integration."""
+"""Config flow for Eaton xComfort Bridge."""
 import logging
-from typing import Any
+from typing import List, Optional
 
-
+from aiohttp import ClientConnectionError
 import voluptuous as vol
 
 from homeassistant import config_entries
-from homeassistant.core import HomeAssistant
-from homeassistant.data_entry_flow import FlowResult
-from homeassistant.exceptions import HomeAssistantError
+from homeassistant.const import (
+    CONF_MONITORED_CONDITIONS,
+    CONF_PASSWORD,
+    CONF_USERNAME,
+)
+from homeassistant.core import callback
+from homeassistant.helpers import aiohttp_client, config_validation as cv
+from homeassistant.helpers.typing import ConfigType
 
-from .const import DOMAIN
+from .const import CONF_IDENTIFIER, DOMAIN, CONF_HOST
 
 _LOGGER = logging.getLogger(__name__)
 
-# TODO adjust the data schema to the data that you need
-STEP_USER_DATA_SCHEMA = vol.Schema(
-    {
-        vol.Required("host"): str,
-        vol.Required("username"): str,
-        vol.Required("password"): str,
-    }
-)
 
-
-class PlaceholderHub:
-    """Placeholder class to make tests pass.
-
-    TODO Remove this placeholder class and replace with things from your PyPI package.
-    """
-
-    def __init__(self, host: str) -> None:
-        """Initialize."""
-        self.host = host
-
-    async def authenticate(self, username: str, password: str) -> bool:
-        """Test if we can authenticate with the host."""
-        return True
-
-
-async def validate_input(hass: HomeAssistant, data: dict[str, Any]) -> dict[str, Any]:
-    """Validate the user input allows us to connect.
-
-    Data has the keys from STEP_USER_DATA_SCHEMA with values provided by the user.
-    """
-    # TODO validate the data can be used to set up a connection.
-
-    # If your PyPI package is not built with async, pass your methods
-    # to the executor:
-    # await hass.async_add_executor_job(
-    #     your_validate_func, data["username"], data["password"]
-    # )
-
-    hub = PlaceholderHub(data["host"])
-
-    if not await hub.authenticate(data["username"], data["password"]):
-        raise InvalidAuth
-
-    # If you cannot connect:
-    # throw CannotConnect
-    # If the authentication is wrong:
-    # InvalidAuth
-
-    # Return info that you want to store in the config entry.
-    return {"title": "Name of the device"}
-
-class HiteProConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
-    """Handle a config flow for Hite-Pro Bridge."""
+@config_entries.HANDLERS.register(DOMAIN)
+class HiteProBridgeConfigFlow(config_entries.ConfigFlow):
 
     VERSION = 1
+    CONNECTION_CLASS = config_entries.CONN_CLASS_LOCAL_PUSH
 
-    async def async_step_user(
-        self, user_input: dict[str, Any] | None = None
-    ) -> FlowResult:
-        """Handle the initial step."""
-        errors: dict[str, str] = {}
+    def __init__(self):
+        self.data = {}
+
+    async def async_step_user(self, user_input=None):
+
+        entries = self.hass.config_entries.async_entries(DOMAIN)
+        if entries:
+            return self.async_abort(reason="already_setup")
+
+        errors = {}
+
         if user_input is not None:
-            try:
-                info = await validate_input(self.hass, user_input)
-            except CannotConnect:
-                errors["base"] = "cannot_connect"
-            except InvalidAuth:
-                errors["base"] = "invalid_auth"
-            except Exception:  # pylint: disable=broad-except
-                _LOGGER.exception("Unexpected exception")
-                errors["base"] = "unknown"
-            else:
-                return self.async_create_entry(title=info["title"], data=user_input)
+
+            self.data[CONF_HOST] = user_input[CONF_HOST]
+            self.data[CONF_USERNAME] = user_input[CONF_USERNAME]
+            self.data[CONF_PASSWORD] = user_input[CONF_PASSWORD]
+            self.data[CONF_IDENTIFIER] = user_input.get(CONF_IDENTIFIER)
+
+            await self.async_set_unique_id(self.data[CONF_HOST])
+
+            return self.async_create_entry(
+                title=f"{user_input[CONF_HOST]}",
+                data=user_input,
+            )
+
+        data_schema = {
+            vol.Required(CONF_HOST): str,
+            vol.Required(CONF_USERNAME): str,
+            vol.Required(CONF_PASSWORD): str,
+            vol.Optional(CONF_IDENTIFIER, default="Hite-Pro Bridge"): str,
+        }
 
         return self.async_show_form(
-            step_id="user", data_schema=STEP_USER_DATA_SCHEMA, errors=errors
+            step_id="user", data_schema=vol.Schema(data_schema), errors=errors
         )
 
-
-class CannotConnect(HomeAssistantError):
-    """Error to indicate we cannot connect."""
-
-
-class InvalidAuth(HomeAssistantError):
-    """Error to indicate there is invalid auth."""
+    async def async_step_import(self, import_data: dict):
+        return await self.async_step_user(import_data)
--- custom_components/hite_pro/const.py
+++ custom_components/hite_pro/const.py
@@ -1,6 +1,7 @@
 """Constants for the Hite-Pro Bridge integration."""
 
 DOMAIN = "hite_pro"
+CONF_AUTH_KEY = "auth_key"
 CONF_IDENTIFIER = "identifier"
 CONF_HOST = "host"
 CONF_USERNAME = "username"
